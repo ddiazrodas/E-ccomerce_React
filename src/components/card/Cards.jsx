81d096b22a5edfea55c58fcfa61c62e9b427894a
Diego Diaz Rodas <diegodiazrodas@gmail.com>
2022-07-12 23:05:59 -0300

    agregando eventos en el ItemDetail

--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -3,38 +3,45 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions } from '@mui/material';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 import { Link } from 'react-router-dom';
+import { CardActionArea, CardActions } from '@mui/material';
 // import auricular from '../../assets/imgs/auricular.png';
 import { ItemCount } from '../itemCount/ItemCount';
 import './cards.scss';
 
 
-function Cards ({product}) {
+function Cards({ product }) {
 
-
-    const { id, pictureURL,  title, description, price} = product
+    const { id, pictureURL, title, description, price, stock } = product
 
     return (
-        <Card className='card' sx={{ maxWidth: 345, height: 770 }}>
+        <Card className='card' sx={{ maxWidth: 345, height: 900 }}>
             <CardActionArea>
                 <CardMedia
                     component="img"
                     height="400"
                     image={pictureURL}
-                    alt="auricular" /> 
+                    alt="auricular" />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                    {title} -$ {price}
+                        {title}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="div">
+                        $ {price}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                    {description}
+                        {description}
                     </Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions>
-                <ItemCount stock={ 10 } initial={ 1 }/>
-                <Link to={`/detail/${id}`}>Ver detalle</Link>
+            <CardActions className='btn-add-cart'>
+                <ItemCount stock={stock} initial={1} />
+                <div className='btn-add-cart2 position-absolute bottom-0'>
+                    <Button className='mb-4' variant='contained' color='primary' endIcon={<SendIcon />}>Agregar al carrito</Button>
+                    <Link className='ver-detalle' to={`/detail/${id}`}>Ver detalle</Link>
+                </div>
             </CardActions>
         </Card>
     );
--- src/mocks/fakeApi.js
+++ src/mocks/fakeApi.js
@@ -5,6 +5,7 @@ const products = [
     description:
       "Con la tecnología EAR (Ergonomic Acoustic Refinement), GAME ZERO ofrece la máxima precisión y claridad de audio. El diseño acústico cerrado proporcionado por las rejillas de los auriculares de acero pintado a medida le permite escuchar hasta el más mínimo detalle de su juego.",
     price: 43000,
+    stock: 200,
     category: "auriculares",
     pictureURL:
       "https://http2.mlstatic.com/D_NQ_NP_654010-MLA31127875096_062019-O.webp",
@@ -16,6 +17,7 @@ const products = [
     description:
       "¡Experimentá la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los Sennheiser GSP 300 no te perdés ningún detalle y escuchás el audio tal y como fue diseñado por los creadores.",
     price: 26000,
+    stock: 198,
     category: "auriculares",
     pictureURL:
       "https://http2.mlstatic.com/D_NQ_NP_672852-MLA41244303335_032020-O.webp",
@@ -26,6 +28,7 @@ const products = [
     description:
       "Nuestros legendarios auriculares para juegos, GAME ONE, se encuentran entre los auriculares favoritos para jugadores de todo el mundo. La tecnología original de los altavoces y un micrófono superlativo con cancelación de ruido ofrecen un rendimiento de alta fidelidad excepcional.",
     price: 46000,
+    stock: 182,
     category: "auriculares",
     pictureURL:
       "https://http2.mlstatic.com/D_NQ_NP_2X_817334-MLA31094927239_062019-F.webp",
@@ -36,6 +39,7 @@ const products = [
     description:
       "Los auriculares inalámbricos para juegos GSP 370 ofrecen audio confiable y sin demoras con hasta 100 horas de juego con una sola carga. Personalice el audio de su juego a través de EPOS Gaming Suite con procesamiento de sonido envolvente digital 7.1. El auricular cuenta con un micrófono con un brazo articulado flexible que se puede levantar para silenciar y una gran comodidad de uso",
     price: 66000,
+    stock: 124,
     category: "auriculares",
     pictureURL:
       "https://http2.mlstatic.com/D_NQ_NP_2X_837834-MLA45400087977_032021-F.webp",
@@ -72,7 +76,6 @@ export const getData = (categoryId) => {
   return new Promise((resolve, reject) => {
 
     const productsFiltrados = products.filter(prod => prod.category === categoryId)
-
 
     setTimeout(() => {
       (categoryId) ? resolve (productsFiltrados) : resolve(products);
